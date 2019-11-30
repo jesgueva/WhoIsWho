95023cb342c37fd8854d9f65fc3e9fc2467f2f6f
jesgueva <jesgueva@gmail.com>
2019-11-29 19:05:33 -0500

    NIT

---
+++ cmake-build-debug/CMakeFiles/WhoIsWho.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/WhoIsWho.dir/testing.cpp.o"
+  "WhoIsWho.exe"
+  "WhoIsWho.pdb"
+  "libWhoIsWho.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/WhoIsWho.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ testing.cpp
@@ -0,0 +1,10 @@
+//
+// Created by esguevaj on 11/29/19.
+//
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    cout << "Hello World\n";
+}
